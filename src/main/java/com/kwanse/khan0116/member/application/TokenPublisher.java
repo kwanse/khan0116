045a0eafc3706ae98faa9169c4f6ffd636873865
kwanse <khan67781397@gmail.com>
2025-01-16 17:26:11 +0900

    fix: 질문 기능 컴파일 에러 제거

--- src/main/java/com/kwanse/khan0116/member/application/TokenPublisher.java
+++ src/main/java/com/kwanse/khan0116/member/application/TokenPublisher.java
@@ -1,7 +1,9 @@
 package com.kwanse.khan0116.member.application;
 
 import com.kwanse.khan0116.member.domain.Role;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface TokenPublisher {
     String createToken(Long memberId, Role role);
 }
