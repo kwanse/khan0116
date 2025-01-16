5826ed25573ba3fb5c1f7666e25ebbc34ce178f3
kwanse <khan67781397@gmail.com>
2025-01-16 13:24:19 +0900

    Initial commit

---
+++ src/main/java/com/kwanse/khan0116/Khan0116Application.java
@@ -0,0 +1,13 @@
+package com.kwanse.khan0116;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Khan0116Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Khan0116Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kwanse/khan0116/Khan0116ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwanse.khan0116;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Khan0116ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
